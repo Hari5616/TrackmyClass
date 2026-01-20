c3d131dcf29c3098cd19bef940efcc7543e6446e
Hari5616 <kvumaramesh78@gmail.com>
2026-01-20 20:01:35 +0530

    UPDATED

--- Project/database/connection.py
+++ Project/database/connection.py
@@ -1,8 +1,17 @@
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+load_dotenv()
 #Need toconnnect Mongo Db and return ..database..
 def get_database():
-    pass
+    client= MongoClient(os.getenv("MONGO_URL"))
+    return client['TrackMyClass']
+    
 
 #Get a specific collection from database, arguement is the collections name
-def get_collection():
-    pass
+def get_collection(name):
+    db=get_database()
+    return db[name]
+
+
 
--- Project/database/operations.py
+++ Project/database/operations.py
@@ -1,24 +1,52 @@
+from connection import get_collection
+
 #arguent are collection_name and the document we want to insert, return the inserted_id
-def insert_one():
-    pass
+def insert_one(collection_name,document):
+    collection=get_collection(collection_name)
+    result=collection.insert_one(document)
+    return result.inserted_id
 
 #same as above
-def insert_many():
-    pass
+def insert_many(collection_name,document):
+    collection=get_collection(collection_name)
+    result=collection.insert_many(document)
+    return result.inserted_id
+    
 
 #Find one document in collection,arguement is collection_name and querry
-def find_one():
-    pass
+def find_one(collection_name,query):
+    collection=get_collection(collection_name)
+    return collection.find_one(query)
+    
 
 #Find multiple documents in collection , collection_name,query (something is there , we need to list it)
-def find_many():
-    pass
+def find(collection_name,query):
+    collection=get_collection(collection_name)
+    if query is None:
+        query={}
+    return list(collection.find(query))
+    
 
 #Update one document in collection, arguements are collection_name,query,update_name
-def update_one():
-    pass
+def update_one(collection_name,query,update_value):
+    collection=get_collection(collection_name)
+    return collection.update_one(query,update_value)
+    
 
 #Delete one document from collection,collection_name,query
-def delete_one():
-    pass
+def delete_one(collection_name,query):
+    collection=get_collection(collection_name)
+    return collection.delete_one(query)
 
+
+sample_student = {
+    "roll_no": "2026CS101",
+    "name": "Rahul Kumar",
+    "branch": "CSE",
+    "year": 1,
+    "email": "rahul.k@iiits.in",
+    "subjects": ["C Programming", "Digital Logic", "Math"],
+    "is_active": True
+}
+
+print(insert_one('students',sample_student))
