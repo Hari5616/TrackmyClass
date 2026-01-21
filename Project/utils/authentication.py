3d5355e31b06d8603ab82ad4b839659de164d251
Hari5616 <kvumaramesh78@gmail.com>
2026-01-22 01:32:39 +0530

    Update authentication.py

--- Project/utils/authentication.py
+++ Project/utils/authentication.py
@@ -1,9 +1,21 @@
-#def hash_password():
- #   pass
+import bcrypt
+from database.operations import find_one
 
-#def verify_password():
-  #  pass
+#We use this when we seed the data
+def hash_password(plain_text):
+  salt=bcrypt.gensalt()
+  return bcrypt.hashpw(plain_text.encode('utf-8'),salt)
 
-#def login():
- #   pass
+def verify_password(plain_text,password_hashed):
+  return bcrypt.checkpw(plain_text.encode('utf-8'),password_hashed) 
 
+def login(student_id,password):
+  student=find_one('students',{'student_id':student_id})
+  if student and verify_password(password,student['password']):
+    student.pop('password',None)
+    student.pop('_id',None)
+  return student
+  
+print(login('STU002','password123'))
+
+#plain_text.encode('utf-8') for making it to byte form for brcypt
