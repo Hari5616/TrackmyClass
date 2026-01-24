86a6905c084f5ed154051b948b1d44a3e30410fb
Hari5616 <kvumaramesh78@gmail.com>
2026-01-24 19:04:04 +0530

    Update authentication.py

--- Project/utils/authentication.py
+++ Project/utils/authentication.py
@@ -16,6 +16,4 @@ def login(student_id,password):
     student.pop('_id',None)
   return student
   
-print(login('STU002','password123'))
-
 #plain_text.encode('utf-8') for making it to byte form for brcypt
